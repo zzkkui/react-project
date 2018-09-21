a7b1d44a464cfc7d1eb083b728dff05e9ad5c4b5
Quintus <1321095399@qq.com>
2018-09-21 18:51:29 +0800

    header 改造成无状态组件

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -13,53 +13,47 @@ import {
 } from './style';
 import { searchInputFocused, searchInputBlur } from '../../store/actionCreator';
 
-class Header extends Component {
-
-  render() {
-    return (
-      <Fragment>
-        <HeaderWrapper>
-          <Logo></Logo>
-          <Nav>
-            <NavItem className='left active'>首页</NavItem>
-            <NavItem className='left'>下载APP</NavItem>
-            <NavItem className='right'>登陆</NavItem>
-            <NavItem className='right'>
-              <i className='iconfont'>&#xe636;</i>
-            </NavItem>
-            <SearchWrapper>
-              <CSSTransition 
-                in={this.props.focused}
-                timeout={300}
-                classNames='slide'>
-                <NavSearch 
-                  className={this.props.focused ? 'focused' : ' '}
-                  onFocus={this.props.searchInputFocused}
-                  onBlur={this.props.searchInputBlur}
-                ></NavSearch>
-              </CSSTransition> 
-              <CSSTransition 
-                in={this.props.focused}
-                timeout={300}
-                classNames='fade'>         
-                <i className={this.props.focused ? 'focused iconfont' : 'iconfont'}>&#xe60c;</i>
-              </CSSTransition>
-            </SearchWrapper>
-            
-          </Nav>
-          <Addition>
-            <Button className='writing'>
-            <i className='iconfont'>&#xe62b;</i>
-              写文章
-            </Button>
-            <Button className='reg'>注册</Button>        
-          </Addition>
-        </HeaderWrapper>
-      </Fragment>
-      
-    )
-  }
-}
+const Header = (props) => (
+  <Fragment>
+    <HeaderWrapper>
+      <Logo></Logo>
+      <Nav>
+        <NavItem className='left active'>首页</NavItem>
+        <NavItem className='left'>下载APP</NavItem>
+        <NavItem className='right'>登陆</NavItem>
+        <NavItem className='right'>
+          <i className='iconfont'>&#xe636;</i>
+        </NavItem>
+        <SearchWrapper>
+          <CSSTransition 
+            in={props.focused}
+            timeout={300}
+            classNames='slide'>
+            <NavSearch 
+              className={props.focused ? 'focused' : ' '}
+              onFocus={props.searchInputFocused}
+              onBlur={props.searchInputBlur}
+            ></NavSearch>
+          </CSSTransition> 
+          <CSSTransition 
+            in={props.focused}
+            timeout={300}
+            classNames='fade'>         
+            <i className={props.focused ? 'focused iconfont' : 'iconfont'}>&#xe60c;</i>
+          </CSSTransition>
+        </SearchWrapper>
+        
+      </Nav>
+      <Addition>
+        <Button className='writing'>
+        <i className='iconfont'>&#xe62b;</i>
+          写文章
+        </Button>
+        <Button className='reg'>注册</Button>        
+      </Addition>
+    </HeaderWrapper>
+  </Fragment>
+)
 
 const mapStateToProps = (state, ownProps) => {
   return {
